52977995dc92163b30bf12c745048ed11eb0164f
herokato <h_htb_atm_k@yahoo.co.jp>
2016-03-27 12:53:50 +0000

    add ranking 3

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,13 +2,13 @@ class RankingController < ApplicationController
   
   def have
     @rankings = Have.group(:item_id).order('count_id desc').limit(10).count(:id)
-    @ranking_item = Item.all
+    @rankings.transform_keys! { |k| Item.find(k) } 
     @type = "Have"
   end
 
   def want
     @rankings = Want.group(:item_id).order('count_id desc').limit(10).count(:id)
-    @ranking_item = Item.all
+    @rankings.transform_keys! { |k| Item.find(k) } 
     @type = "Want"
   end
 
