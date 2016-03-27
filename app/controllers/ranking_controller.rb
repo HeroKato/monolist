30dd44009cdbacac2d285008ed13b77329c8ff4e
herokato <h_htb_atm_k@yahoo.co.jp>
2016-03-27 09:26:56 +0000

    add ranking 2

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,12 +1,14 @@
 class RankingController < ApplicationController
   
   def have
-    @rankings = Have.group(:item_id).order('item_id desc').limit(10).count(:id)
+    @rankings = Have.group(:item_id).order('count_id desc').limit(10).count(:id)
+    @ranking_item = Item.all
     @type = "Have"
   end
 
   def want
-    @rankings = Want.group(:item_id).order('item_id desc').limit(10).count(:id)
+    @rankings = Want.group(:item_id).order('count_id desc').limit(10).count(:id)
+    @ranking_item = Item.all
     @type = "Want"
   end
 
